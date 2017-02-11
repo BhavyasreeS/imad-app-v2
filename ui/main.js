3f8776e28d9ebfa23f9006f55991d1d3b0c7ed5a
BhavyasreeS <sbhavya1515@gmail.com>
2017-02-11 22:41:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ button.onclick=function() {
             if(request.status===200){
                 var counter=request.resposeText;
                 var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+                span.innerHTML= counter.toString();
             }
         }
         //Not done
