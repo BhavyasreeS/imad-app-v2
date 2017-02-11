c72d94e31e4480db9c1e470b3d3f1d3387a02f9f
BhavyasreeS <sbhavya1515@gmail.com>
2017-02-11 18:55:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,6 @@ element.innerHTML='New Value' ;
 //move the image
 var img= document.getElementById('madi');
 img.onclick=function() {
-    img.style.marginleft='200px';
+    img.style.marginLeft='200px';
     
 };
