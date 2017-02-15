1652b7b9d7ef43ac01bc46ad68b627128ac941e3
BhavyasreeS <sbhavya1515@gmail.com>
2017-02-15 20:39:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,12 +99,12 @@ return htmlTemplate;
 
 
 
-var counter=0;
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
